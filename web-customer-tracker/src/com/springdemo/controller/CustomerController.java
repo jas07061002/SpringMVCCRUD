5ed4820b26c94676377a378d8dc4d4f8b83e3d30
jas07061002 <jas18kalra@gmail.com>
2019-05-06 20:11:26 -0400

    Added a service layer to the application

--- web-customer-tracker/src/com/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/springdemo/controller/CustomerController.java
@@ -5,25 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.springdemo.dao.CustomerDAO;
 import com.springdemo.entity.Customer;
+import com.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// need to inject the customer DAO 
-	
+	//need to inject our customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
 		// get customers from the DAO
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		// add the customers to the model
 		theModel.addAttribute("customers",theCustomers);

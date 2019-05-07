22090c73894db82dff3d6b6c702c9a8ebfeab934
jas07061002 <jas18kalra@gmail.com>
2019-05-07 16:14:54 -0400

    Update feature added

--- web-customer-tracker/src/com/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/springdemo/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springdemo.entity.Customer;
 import com.springdemo.service.CustomerService;
@@ -53,6 +54,23 @@ public class CustomerController {
 		customerService.saveCustomer(theCustomer);
 		
 		return "redirect:/customer/list";
+	}
+	
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId,
+			Model theModel) {
+		
+		// get the customer from our service
+		Customer theCustomer = customerService.getCustomer(theId);
+		
+		// set the customer as a model attribute to pre-populate the form
+		theModel.addAttribute("customer",theCustomer);
+	
+		// send over our form
+		
+		return "customer-form";
+		
 	}
 	
 	
--- web-customer-tracker/src/com/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/com/springdemo/dao/CustomerDAOImpl.java
@@ -26,7 +26,8 @@ public class CustomerDAOImpl implements CustomerDAO {
 		
 		// create a query
 		Query<Customer> theQuery = 
-				currentSession.createQuery("from Customer", Customer.class);
+				currentSession.createQuery("from Customer order by lastName"
+						+ "", Customer.class);
 		
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
@@ -42,9 +43,22 @@ public class CustomerDAOImpl implements CustomerDAO {
 		// get the current hibenrate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		// save the customer
-		currentSession.save(theCustomer);
+		// save/update the customer
+		currentSession.saveOrUpdate(theCustomer);
 		
+	}
+
+
+	@Override
+	public Customer getCustomer(int theId) {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// now retrieve / read from db using primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
 	}
 
 }
